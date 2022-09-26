32e8944b85467402471daa8b47eaf783f10ecbed
michael-kestler <michael.s.kestler@gmail.com>
2022-09-25 20:41:01 -0700

    Refactored exercise to account for alphanumeric characters

--- charCount.js
+++ charCount.js
@@ -6,25 +6,26 @@
 
 function charCount(str){
 //make object to return at end
-var result = {};
+var obj = {};
 
 //loop over string, for each character
 for(var i = 0; i < str.length; i++){
 
 //if the char is a number/letter AND is a key in the object, add one to count,
   var char = str[i].toLowerCase()
-  if(result[char] > 0){
-    result[char]++;
-  }
+  if(/[a-z0-9]/.test(char)){
+    if(obj[char] > 0){
+      obj[char]++;
+    }   else {
+      obj[char] = 1;
+  };
   //if the char is a number/letter AND is not in object, add it and set value to 1
-  else {
-    result[char] = 1;
+
   }
 
   //if character is something else (space, period, etc.) don't do anything
-
 }
 //return object
-return result;
+return obj;
 
 }
