c3d58afe4f3cebbf4954e1b69bdd2076a5b75ebf
michael-kestler <michael.s.kestler@gmail.com>
2022-09-25 20:54:39 -0700

    added function using char codes to determine if char is alphanumeric

--- charCount.js
+++ charCount.js
@@ -29,3 +29,13 @@ for(var i = 0; i < str.length; i++){
 return obj;
 
 }
+
+function isAlpaNumeric(char){
+  var code = char.charCodeAt(0);
+  if(!(code > 47 && code < 58)) && //numeric (0-9)
+   !(code > 64 && code < 91) && // upper alpha (A-Z)
+   !(code > 96 && code < 123)) { // lower alpha (a-z)
+     return false;
+   }
+   return true;
+}
