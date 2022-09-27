4382f231618caa4d27a27070881c47fa1dc7e831
michael-kestler <michael.s.kestler@gmail.com>
2022-09-27 13:41:38 -0700

    refactored anagram

--- anagram.js
+++ anagram.js
@@ -21,3 +21,26 @@ if (str1 === str2){
 }
 
 }
+
+
+
+//refactored
+function validAnagram(a, b){
+
+  let len1 = a.length;
+  let len2 = b.length;
+  let str1 = a.split('').sort().join('');
+  let str2 = b.split('').sort().join('');
+
+
+if (len1 !== len2){
+   return false;
+} else if (str1 === str2){
+    return true;
+
+} else {
+    return false;
+
+}
+
+}
