89e69bff2fa7b55a6c528094ca654328bb15ed9d
michael-kestler <michael.s.kestler@gmail.com>
2022-10-02 22:19:55 -0700

    Added comments

--- factorsOfNumber.js
+++ factorsOfNumber.js
@@ -4,6 +4,16 @@ function factorsOfNumber(number){
   for(let i = 1; i <= number; i++){
     if(number % i == 0){
       console.log (i);
+      //prints factors of number
     }
   }
 }
+
+factorsOfNumber(75);
+//prints
+// 1
+// 3
+// 5
+// 15
+// 25
+// 75
