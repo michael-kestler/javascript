b7e4bab94b4988c30d2d82b8b2bc87fc9d314981
michael-kestler <michael.s.kestler@gmail.com>
2022-10-04 22:54:11 -0700

    created function that counts occurrence of character input in string

---
+++ searchedCharacter.js
@@ -0,0 +1,13 @@
+
+function findCharCount(str, c){
+
+  let len = str.length;
+  let count = 0;
+
+  for(let i = 0; i<=len; i++){
+    if(arr[i] === c){
+      count++;
+    }
+  }
+  console.log(count);
+}
