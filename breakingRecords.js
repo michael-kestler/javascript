d1ba8d8ccfac3b1869601a25ebf13902b0f5c760
michael-kestler <michael.s.kestler@gmail.com>
2022-10-11 23:51:50 -0700

    Completed hackerrank exercise

---
+++ breakingRecords.js
@@ -0,0 +1,33 @@
+//Maria plays college basketball and wants to go pro.
+//Each season she maintains a record of her play.
+//She tabulates the number of times she breaks her season record for most points and least points in a game. Points scored in the first game establish her record
+//for the season, and she begins counting from there.
+
+// Given the scores for a season, determine the number
+//  of times Maria breaks her records
+// for most and least points scored during the season.
+
+
+
+function breakingRecords(scores) {
+    // Write your code here
+    let minCount = 0;
+    let maxCount = 0;
+    let minScore = scores[0];
+    let maxScore = scores[0];
+    let maxMin = [];
+    for(let i=0; i<scores.length; i++){
+        if(scores[i] > maxScore){
+            maxScore = scores[i];
+            maxCount++;
+
+        } else if(scores[i] < minScore){
+            minScore = scores[i];
+            minCount++;
+
+        }
+    }
+
+
+    return[maxCount, minCount];
+}
